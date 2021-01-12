b57f20d7c3a05f9390f0bd2b51c9ca86d2515ca2
Eugene <efurer@gmail.com>
2021-01-12 08:55:36 -0500

    added input args handling

--- app.py
+++ app.py
@@ -32,6 +32,7 @@ def get_vendor(mac):
         return "(Vendor Lookup Error)"
 
 def refresh_vendors():
+    print ("==== Starting Venfor Refresh ====")
     with open('db.json','r') as db_file:
         db=json.load(db_file)
         for mac in db:
@@ -40,65 +41,67 @@ def refresh_vendors():
             print (mac + " : " device["vendor"])
     with open ("db.json","w") as db_file:
         db_file.write(json.dumps(db,indent=4))
-    print ("Vendors refreshed")
+    print ("==== Vendors refresh finished ====")
     
+
 if __name__=='__main__':
-    print("__main__")
 
-
-
-startIP = 1
-endIP = 255
-if len(sys.argv)>1:
-    for key in sys.argv:
-        print (key)
-        if (key=="rv"):
-            refresh_vendors()
-            print ("Vendors refrshed")
-            sys.exit()
-        
-    startIP=int(sys.argv[1])
-if len(sys.argv)>2:
-    endIP=int(sys.argv[2])
-#print (str(sys.argv))
-
-odevices = {}
-print ("Scan Started 192.168.1." + str(startIP) + " - 192.168.1."+ str(endIP) +"\n=========================\n")
-
-for i in range(startIP,(endIP+1)):
-    ip = "192.168.1." +str(i)
-    mac=""
-    status = 0
-    pingResponse = os.system("ping -c1 -w1 "+ ip + " > /dev/null 2>&1")
-    if pingResponse == 0:
-        mac = get_mac(ip)
-        odevices[mac.upper()]=ip
-        status=1
-    print (ip + " " + (mac if status==1 else "offline"))
-
-with open('db.json','r') as db_file:
-    db=json.load(db_file)
-
-
-    #insert missing
-    print ("Scan Finished\nOnline Devices")
-    for mac in odevices:
-        print (mac + " @ " + odevices[mac])
-        if mac not in db:
-            db[mac]={"ip":odevices[mac],"description":"","vendor":"","status":1,"host":""}
-            print("creating new " + mac + ":" + odevices[mac])
+    startIP = 1
+    endIP = 255
+    if len(sys.argv)>1:
+        i=0
+        while i < len(sys.argv):
+            print (key[i])
+            if key[i]=="-rv":
+                refresh_vendors()
+                sys.exit()
+            if key[i]="-?":
+                print ("---Help will be displayed here----")
+                sys.exit()
+            if key[i]=="-s":
+                i+=1
+                startIP=int(key[i])
+            if key[i]=="-e":
+                i+=1
+                endIP=int(key[i])
+            
     
-    # Update
-    for mac in db:
-        device=db[mac]
-        if mac in odevices:
-            device["status"]=1
-            device["ip"]=odevices[mac]
-            if device["vendor"]=="":
-                device["vendor"]=get_vendor(mac)
-        else:
-            device["status"]=0
-with open ("db.json","w") as db_file:
-    db_file.write(json.dumps(db,indent=4))
-print("db.json updated")
+
+    odevices = {}
+    print ("Scan Started 192.168.1." + str(startIP) + " - 192.168.1."+ str(endIP) +"\n=========================\n")
+    # Pinging IPs within specified range
+    for i in range(startIP,(endIP+1)):
+        ip = "192.168.1." +str(i)
+        mac=""
+        status = 0
+        pingResponse = os.system("ping -c1 -w1 "+ ip + " > /dev/null 2>&1")
+        if pingResponse == 0:
+            mac = get_mac(ip)
+            odevices[mac.upper()]=ip
+            status=1
+        print (ip + " " + (mac if status==1 else "offline"))
+
+    with open('db.json','r') as db_file:
+        db=json.load(db_file)
+        #insert missing
+        print ("Scan Finished\nOnline Devices")
+        for mac in odevices:
+            print (mac + " @ " + odevices[mac])
+            if mac not in db:
+                db[mac]={"ip":odevices[mac],"description":"","vendor":"","status":1,"host":""}
+                print("creating new " + mac + ":" + odevices[mac])
+
+        # Update 
+        for mac in db:
+            device=db[mac]
+            if mac in odevices:
+                device["status"]=1
+                device["ip"]=odevices[mac]
+                if device["vendor"]=="":
+                    device["vendor"]=get_vendor(mac)
+            else:
+                device["status"]=0
+    with open ("db.json","w") as db_file:
+        db_file.write(json.dumps(db,indent=4))
+    print("db.json updated")
     
