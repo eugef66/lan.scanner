4ebb0c85db057d91a40cfe1415cc72c8c91c85a8
eugef66 <efurer@gmail.com>
2021-01-31 20:36:24 -0500

    backup

--- app.py
+++ app.py
@@ -2,6 +2,14 @@
 import os
 import json
 import sys
+import uuid
+import requests
+import subprocess
+import re
+from subprocess import Popen, PIPE
+
+
+
 
 DB = None
 APP_PATH = os.path.dirname(os.path.abspath(__file__))
@@ -14,9 +22,36 @@ else:
 
 #Scan Devices marked "Alert Down"
 def scan_down():
+    if (DB==None):
+        _load_db()
+    
+#    for mac in DB:
+#        print (mac + " -> " + DB[mac]["description"])
+    
     return
 #Scan for new devices
 def scan_new():
+    if (DB==None):
+        _load_db()
+    
+    print ("--- Starting IP Scan --")
+    
+    for i in range(1,256):
+        ip = IP_MASK +str(i)
+        mac=""
+        is_online = False
+        
+        pingResponse = os.system("ping -c1 -w1 "+ ip + " > /dev/null 2>&1")
+        if pingResponse == 0:
+            mac = get_mac_by_ip(ip)
+            is_online=True
+            if (mac not in DB):
+                create_update_device(False,mac,ip,is_online,"",False,"")
+            else:
+                create_update_device(False,mac,ip,is_online,DB[mac]["description"],DB[mac]["alert_down"],DB[mac]["hostname"])
+
+
+    _save_db()
     return
 
 # Get Vendor Name by MAC
@@ -44,14 +79,43 @@ def get_mac_by_ip(ip):
      finally:
         return mac
 
-def load_config():
+def create_update_device(saveto_db, mac,ip,is_online,description, alert_down,hostname):
+    if DB==None:
+        _load_db()
+    
+    
+    #TODO: if description or hostname is empty get from pihole
+    vendor=""
+    if (mac in DB):
+        vendor = DB[mac]["vendor"]
+    else:
+        vendor = get_vendor_by_mac(mac)
+
+    DB[mac]={"ip":ip,
+            "is_online":is_online,
+            "description":description,
+            "vendor":vendor,
+            "hostname":hostname,
+            "alert_down":alert_down
+            }
+    print ("--- Creating New Device ---")
+    print (DB[mac])
+    if (saveto_db):
+        _save_db()
     return
 
-def load_db():
-    with open(APP_PATH + '/db.json','r') as db_file:
-        DB=json.load(db_file)
+def _load_db():
+    print ("--- loading database ---")
+    global DB
+    if (os.path.exists(APP_PATH + "/db.json")):
+        with open(APP_PATH + '/db.json','r') as db_file:        
+            DB=json.load(db_file)
+    else:
+        DB={}
+        _save_db()
+
     return
-def save_db():
+def _save_db():
     with open (APP_PATH + "/db.json","w+") as db_file:
         db_file.write(json.dumps(DB,indent=4))
     return
@@ -59,6 +123,7 @@ def save_db():
 
 #Default process when no method argument provides
 def main():
+    _load_db()
     scan_down()
     scan_new()
 
