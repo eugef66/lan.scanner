746264fbd353d4bdeda8be25b75cf8eca4a7b5b4
Eugene Furer <eugene.furer@gfk.com>
2023-07-19 15:36:18 -0400

    backup code

--- ui/admin.php
+++ ui/admin.php
@@ -2,6 +2,9 @@
 require 'header.php';
 require 'notification.php';
 
+
+
+
 ?>
 
 
@@ -12,8 +15,7 @@ require 'notification.php';
 
 	<!-- Content header--------------------------------------------------------- -->
 	<section class="content-header">
-		<?php require 'notification.php'; ?>
-
+		
 		<h1 id="pageTitle">
 			Device Details
 		</h1>
@@ -21,135 +23,167 @@ require 'notification.php';
 		<!-- Main content ---------------------------------------------------------- -->
 		<section class="content">
 
-<!-- form -->
-<form action="updateMetadata.php" method='POST' id="adminForm">
-			<!-- tab control------------------------------------------------------------ -->
-			<div class="row">
-				<div class="col-lg-12 col-sm-12 col-xs-12">
-					<!-- <div class="box-transparent"> -->
-					<div class="row">
-						<!-- column 1 -->
-						<div class="col-lg-6 col-sm-6 col-xs-12">
-							<h4 class="bottom-border-aqua">Metadata</h4>
-							<div class="box-body form-horizontal">
+			<!-- form -->
+			<form  method='POST' id="adminForm">
+				<!-- tab control------------------------------------------------------------ -->
+				<div class="row">
+					<div class="col-lg-12 col-sm-12 col-xs-12">
+						<!-- <div class="box-transparent"> -->
+						<div class="row">
+							<!-- column 1 -->
+							<div class="col-lg-6 col-sm-6 col-xs-12">
+								<h4 class="bottom-border-aqua">Metadata</h4>
+								<div class="box-body form-horizontal">
 
-								<!-- Owner -->
-								<div class="form-group">
-									<label class="col-sm-3 control-label">Owner</label>
-									<div class="col-sm-9">
-										<div class="input-group">
-											<input class="form-control" id="owner_value" name="owner" type="text"
-												value="">
-											<div class="input-group-btn">
-												<button type="button" class="btn btn-info dropdown-toggle"
-													data-toggle="dropdown" aria-expanded="false">
-													<span class="fa fa-caret-down"></span></button>
-												<ul id="owner" class="dropdown-menu dropdown-menu-right"></ul>
-												<button type=button class="btn btn-success" id="btnOwnerSave"><i class="fa fa-floppy-o "></i></button>
-												<button type=button class="btn btn-danger" disabled id="btnOwnerDelete"><i class="fa fa-trash"></i></button>
+									<!-- Owner -->
+									<div class="form-group">
+										<label class="col-sm-3 control-label">Owner</label>
+										<div class="col-sm-9">
+											<div class="input-group">
+												<input class="form-control" id="owner_value" name="owner" type="text"
+													value="">
+												<div class="input-group-btn">
+													<button type="button" class="btn btn-info dropdown-toggle"
+														data-toggle="dropdown" aria-expanded="false">
+														<span class="fa fa-caret-down"></span></button>
+													<ul id="owner" class="dropdown-menu dropdown-menu-right"></ul>
+													<button type=button class="btn btn-success" id="btnOwnerSave"><i
+															class="fa fa-floppy-o "></i></button>
+													<button type=button class="btn btn-danger" disabled
+														id="btnOwnerDelete"><i class="fa fa-trash"></i></button>
+												</div>
 											</div>
 										</div>
 									</div>
-								</div>
 
-								<!-- Type -->
-								<div class="form-group">
-									<label class="col-sm-3 control-label">Type</label>
-									<div class="col-sm-9">
-										<div class="input-group">
-											<input class="form-control" id="device_type_value" name="device_type" type="text"
-												value="">
-											<div class="input-group-btn">
-												<button type="button" class="btn btn-info dropdown-toggle"
-													data-toggle="dropdown" aria-expanded="false">
-													<span class="fa fa-caret-down"></span></button>
-												<ul id="device_type" class="dropdown-menu dropdown-menu-right">
-												</ul>
-												<button type=button class="btn btn-success" id="btnTypeSave"><i class="fa fa-floppy-o "></i></button>
-												<button type=button class="btn btn-danger" disabled id="btnTypeDelete"><i class="fa fa-trash"></i></button>
+									<!-- Type -->
+									<div class="form-group">
+										<label class="col-sm-3 control-label">Type</label>
+										<div class="col-sm-9">
+											<div class="input-group">
+												<input class="form-control" id="device_type_value" name="device_type"
+													type="text" value="">
+												<div class="input-group-btn">
+													<button type="button" class="btn btn-info dropdown-toggle"
+														data-toggle="dropdown" aria-expanded="false">
+														<span class="fa fa-caret-down"></span></button>
+													<ul id="device_type" class="dropdown-menu dropdown-menu-right">
+													</ul>
+													<button type=button class="btn btn-success" id="btnTypeSave"><i
+															class="fa fa-floppy-o "></i></button>
+													<button type=button class="btn btn-danger" disabled
+														id="btnTypeDelete"><i class="fa fa-trash"></i></button>
+												</div>
 											</div>
 										</div>
 									</div>
-								</div>
 
 
 
-								<!-- Location -->
-								<div class="form-group">
-									<label class="col-sm-3 control-label">Location</label>
-									<div class="col-sm-9">
-										<div class="input-group">
-											<input class="form-control" id="location_value" name="location" type="text"
-												value="">
-											<div class="input-group-btn">
-												<button type="button" class="btn btn-info dropdown-toggle"
-													data-toggle="dropdown" aria-expanded="false">
-													<span class="fa fa-caret-down"></span></button>
-												<ul id="location" class="dropdown-menu dropdown-menu-right"></ul>
-												<button type=button class="btn btn-success" id="btnLocationSave"><i class="fa fa-floppy-o"></i></button>
-												<button type=button class="btn btn-danger" disabled id="btnLocationDelete"><i class="fa fa-trash"></i></button>
+									<!-- Location -->
+									<div class="form-group">
+										<label class="col-sm-3 control-label">Location</label>
+										<div class="col-sm-9">
+											<div class="input-group">
+												<input class="form-control" id="location_value" name="location"
+													type="text" value="">
+												<div class="input-group-btn">
+													<button type="button" class="btn btn-info dropdown-toggle"
+														data-toggle="dropdown" aria-expanded="false">
+														<span class="fa fa-caret-down"></span></button>
+													<ul id="location" class="dropdown-menu dropdown-menu-right"></ul>
+													<button type=button class="btn btn-success" id="btnLocationSave"><i
+															class="fa fa-floppy-o"></i></button>
+													<button type=button class="btn btn-danger" disabled
+														id="btnLocationDelete"><i class="fa fa-trash"></i></button>
+												</div>
 											</div>
 										</div>
 									</div>
+
+
 								</div>
-
-
 							</div>
-						</div>
 
-						<!-- column 2 -->
-						<div class="col-lg-6 col-sm-6 col-xs-12">
-							<h4 class="bottom-border-aqua">Appirence</h4>
-							<div class="box-body form-horizontal">
+							<!-- column 2 -->
+							<div class="col-lg-6 col-sm-6 col-xs-12">
+								<h4 class="bottom-border-aqua">Appirence</h4>
+								<div class="box-body form-horizontal">
 
-								<!-- Skin -->
-								<!-- Owner -->
-								<div class="form-group">
-									<label class="col-sm-5 control-label">Owner</label>
-									<div class="col-sm-7">
-										<div class="input-group">
-											<input class="form-control" id="skin_value" name="owner" type="text"
-												value="" readonly>
-											<div class="input-group-btn">
-												<button type="button" class="btn btn-info dropdown-toggle"
-													data-toggle="dropdown" aria-expanded="false">
-													<span class="fa fa-caret-down"></span></button>
-												<ul id="skin" class="dropdown-menu dropdown-menu-right">
-													<li><a href="javascript:void(0)" onclick="setTextValue('skin_value','skin-black-light')">Black Light</a></li>
-													<li><a href="javascript:void(0)" onclick="setTextValue('skin_value','skin-black')">Black</a></li>
-													<li><a href="javascript:void(0)" onclick="setTextValue('skin_value','skin-blue-light')">Blue Light</a></li>
-													<li><a href="javascript:void(0)" onclick="setTextValue('skin_value','skin-blue')">Blue</a></li>
-													<li><a href="javascript:void(0)" onclick="setTextValue('skin_value','skin-green-light')">Green Light</a></li>
-													<li><a href="javascript:void(0)" onclick="setTextValue('skin_value','skin-green')">Green</a></li>
-													<li><a href="javascript:void(0)" onclick="setTextValue('skin_value','skin-purple-light')">Purple Light</a></li>
-													<li><a href="javascript:void(0)" onclick="setTextValue('skin_value','skin-purple')">Purple</a></li>
-													<li><a href="javascript:void(0)" onclick="setTextValue('skin_value','skin-red-light')">Red Light</a></li>
-													<li><a href="javascript:void(0)" onclick="setTextValue('skin_value','skin-red')">Red</a></li>
-													<li><a href="javascript:void(0)" onclick="setTextValue('skin_value','skin-yellow-light')">Yellow Light</a></li>
-													<li><a href="javascript:void(0)" onclick="setTextValue('skin_value','skin-yellow')">Yellow</a></li>
-												</ul>
-												<button type=button class="btn btn-primary" id="btnSkinApply"><i class="fa fa-check "></i> Apply</button>
-												
+									<!-- Skin -->
+									<!-- Owner -->
+									<div class="form-group">
+										<label class="col-sm-5 control-label">Owner</label>
+										<div class="col-sm-7">
+											<div class="input-group">
+												<input class="form-control" id="skin_value" name="owner" type="text"
+													value="" readonly>
+												<div class="input-group-btn">
+													<button type="button" class="btn btn-info dropdown-toggle"
+														data-toggle="dropdown" aria-expanded="false">
+														<span class="fa fa-caret-down"></span></button>
+													<ul id="skin" class="dropdown-menu dropdown-menu-right">
+														<li><a href="javascript:void(0)"
+																onclick="setTextValue('skin_value','skin-black-light')">Black
+																Light</a></li>
+														<li><a href="javascript:void(0)"
+																onclick="setTextValue('skin_value','skin-black')">Black</a>
+														</li>
+														<li><a href="javascript:void(0)"
+																onclick="setTextValue('skin_value','skin-blue-light')">Blue
+																Light</a></li>
+														<li><a href="javascript:void(0)"
+																onclick="setTextValue('skin_value','skin-blue')">Blue</a>
+														</li>
+														<li><a href="javascript:void(0)"
+																onclick="setTextValue('skin_value','skin-green-light')">Green
+																Light</a></li>
+														<li><a href="javascript:void(0)"
+																onclick="setTextValue('skin_value','skin-green')">Green</a>
+														</li>
+														<li><a href="javascript:void(0)"
+																onclick="setTextValue('skin_value','skin-purple-light')">Purple
+																Light</a></li>
+														<li><a href="javascript:void(0)"
+																onclick="setTextValue('skin_value','skin-purple')">Purple</a>
+														</li>
+														<li><a href="javascript:void(0)"
+																onclick="setTextValue('skin_value','skin-red-light')">Red
+																Light</a></li>
+														<li><a href="javascript:void(0)"
+																onclick="setTextValue('skin_value','skin-red')">Red</a>
+														</li>
+														<li><a href="javascript:void(0)"
+																onclick="setTextValue('skin_value','skin-yellow-light')">Yellow
+																Light</a></li>
+														<li><a href="javascript:void(0)"
+																onclick="setTextValue('skin_value','skin-yellow')">Yellow</a>
+														</li>
+													</ul>
+													<button type=button class="btn btn-primary" id="btnSkinApply"><i
+															class="fa fa-check "></i> Apply</button>
+
+												</div>
 											</div>
 										</div>
 									</div>
-								</div>
 
-								<!-- Logo -->
-								<div class="form-group">
-									<label class="col-sm-5 control-label">Logo</label>
-									<div class="col-sm-7">
-										<input class="form-control" id="ip" name="ip" type="text" readonly value="--">
+									<!-- Logo -->
+									<div class="form-group">
+										<label class="col-sm-5 control-label">Logo</label>
+										<div class="col-sm-7">
+											<input class="form-control" id="ip" name="ip" type="text" readonly
+												value="--">
+										</div>
 									</div>
+
+
 								</div>
-
-
 							</div>
-						</div>
 
-						
-						<!-- Buttons -->
-						<!--div class="col-xs-12">
+
+							<!-- Buttons -->
+							<!--div class="col-xs-12">
 							<div class="pull-right">
 								<button type="submit" class="btn btn-success pa-btn" style="margin-left:6px; "
 									id="btnSave" name="action" value="save"> Save </button> 
@@ -158,91 +192,94 @@ require 'notification.php';
 							</div>
 						</div-->
 
-					</div>
-					<!-- /.row -->
-					<div class="row">
-						<!-- column 1 -->
-						<div class="col-lg-6 col-sm-6 col-xs-12">
-							<h4 class="bottom-border-aqua">Server Configurations</h4>
-							<div class="box-body form-horizontal">
+						</div>
+						<!-- /.row -->
+						<div class="row">
+							<!-- column 1 -->
+							<div class="col-lg-6 col-sm-6 col-xs-12">
+								<h4 class="bottom-border-aqua">Server Configurations</h4>
+								<div class="box-body form-horizontal">
 
-								<!-- Owner -->
+									<!-- >Alert New Devices -->
+									
 								<div class="form-group">
-									<label class="col-sm-3 control-label">Owner</label>
-									<div class="col-sm-9">
-										<div class="input-group">
-											<input class="form-control" id="owner_value" name="owner" type="text"
-												value="">
-											<div class="input-group-btn">
-												<button type="button" class="btn btn-info dropdown-toggle"
-													data-toggle="dropdown" aria-expanded="false">
-													<span class="fa fa-caret-down"></span></button>
-												<ul id="owner" class="dropdown-menu dropdown-menu-right"></ul>
-												<button type=button class="btn btn-success" id="btnOwnerSave"><i class="fa fa-floppy-o "></i></button>
-												<button type=button class="btn btn-danger" disabled id="btnOwnerDelete"><i class="fa fa-trash"></i></button>
-											</div>
-										</div>
+									<label class="col-sm-3 control-label">Down Device Alert Enabled:</label>
+									<div class="col-sm-9" style="padding-top:6px;">
+										<input class="checkbox blue" id="alert_down" name="alert_down" type="checkbox">
 									</div>
 								</div>
 
-								<!-- Type -->
+
+								<!-- New Device -->
 								<div class="form-group">
-									<label class="col-sm-3 control-label">Type</label>
-									<div class="col-sm-9">
-										<div class="input-group">
-											<input class="form-control" id="device_type_value" name="device_type" type="text"
-												value="">
-											<div class="input-group-btn">
-												<button type="button" class="btn btn-info dropdown-toggle"
-													data-toggle="dropdown" aria-expanded="false">
-													<span class="fa fa-caret-down"></span></button>
-												<ul id="device_type" class="dropdown-menu dropdown-menu-right">
-												</ul>
-												<button type=button class="btn btn-success" id="btnTypeSave"><i class="fa fa-floppy-o "></i></button>
-												<button type=button class="btn btn-danger" disabled id="btnTypeDelete"><i class="fa fa-trash"></i></button>
-											</div>
-										</div>
+									<label class="col-sm-3 control-label">New Device Alert Enabled:</label>
+									<div class="col-sm-9" style="padding-top:6px;">
+										<input class="checkbox blue " id="is_new" name="is_new" type="checkbox">
 									</div>
 								</div>
 
 
 
-								<!-- Location -->
-								<div class="form-group">
-									<label class="col-sm-3 control-label">Location</label>
-									<div class="col-sm-9">
-										<div class="input-group">
-											<input class="form-control" id="location_value" name="location" type="text"
-												value="">
-											<div class="input-group-btn">
-												<button type="button" class="btn btn-info dropdown-toggle"
-													data-toggle="dropdown" aria-expanded="false">
-													<span class="fa fa-caret-down"></span></button>
-												<ul id="location" class="dropdown-menu dropdown-menu-right"></ul>
-												<button type=button class="btn btn-success" id="btnLocationSave"><i class="fa fa-floppy-o"></i></button>
-												<button type=button class="btn btn-danger" disabled id="btnLocationDelete"><i class="fa fa-trash"></i></button>
+									<!-- Alert Down Threshlod -->
+									<div class="form-group">
+										<label class="col-sm-3 control-label">Alert Down Threshlod:</label>
+										<div class="col-sm-9">
+											<div class="input-group">
+												<input class="form-control" id="location_value" name="location"
+													type="text" value="">
 											</div>
 										</div>
 									</div>
+
+									<!-- Alert From -->
+									<div class="form-group">
+										<label class="col-sm-3 control-label">Alert Email From Name:</label>
+										<div class="col-sm-9">
+											<div class="input-group">
+												<input class="form-control" id="location_value" name="location"
+													type="text" value="">
+											</div>
+										</div>
+									</div>
+
+									<!-- Alert Subject -->
+									<div class="form-group">
+										<label class="col-sm-3 control-label">Alert Email Subject:</label>
+										<div class="col-sm-9">
+											<div class="input-group">
+												<input class="form-control" id="location_value" name="location"
+													type="text" value="">
+											</div>
+										</div>
+									</div>
+
+									<!-- Alert To -->
+									<div class="form-group">
+										<label class="col-sm-3 control-label">Alert To Email:</label>
+										<div class="col-sm-9">
+											<div class="input-group">
+												<input class="form-control" id="location_value" name="location"
+													type="text" value="">
+											</div>
+										</div>
+									</div>
+
 								</div>
-
-
 							</div>
-						</div>
 
-						<!-- column 2 -->
-						<div class="col-lg-6 col-sm-6 col-xs-12">
+							<!-- column 2 -->
+							<div class="col-lg-6 col-sm-6 col-xs-12">
+							</div>
+
 						</div>
+						<!-- /.row -->
 
 					</div>
-					<!-- /.row -->
-
+					<!-- /.col -->
 				</div>
-				<!-- /.col -->
-			</div>
-			<!-- /.row -->
-</form>
-<!-- /.form -->
+				<!-- /.row -->
+			</form>
+			<!-- /.form -->
 			<!-- ----------------------------------------------------------------------- -->
 		</section>
 		<!-- /.content -->
@@ -269,15 +306,12 @@ require 'footer.php';
 <link rel="stylesheet" href="lib/AdminLTE/bower_components/fullcalendar/dist/fullcalendar.min.css">
 <link rel="stylesheet" href="lib/AdminLTE/bower_components/fullcalendar/dist/fullcalendar.print.min.css" media="print">
 <script src="lib/AdminLTE/bower_components/moment/moment.js"></script>
-
-<script src="//code.jquery.com/jquery-3.4.1.js"></script>
-<script src="//unpkg.com/bootstrap@4.3.1/dist/js/bootstrap.bundle.min.js"></script>
-
+<script src="lib/AdminLTE/bower_components/fullcalendar/dist/fullcalendar.min.js"></script>
+<script src="utils.js"></script>
 
 <script src="admin.js"></script>
-<script src="utils.js"></script>
+
 
 <script>
 	load();
 </script>
-
